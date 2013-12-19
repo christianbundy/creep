9331bb2175df3193c280aed14e0216df81cf3996
Christian Bundy <me@christianbundy.com>
2013-12-19 12:53:53 -0600

    Update Gemfile, Rakefile, creep, and spec_helper

--- lib/creep.rb
+++ lib/creep.rb
@@ -5,20 +5,34 @@ require 'open-uri'
 class Creep
   attr_reader :data, :success, :failure
 
+  # Initialize creep and merge in the first URI
+  # @param uri [String] the webite to creep
+  # @return [Boolean] if the website can't be scraped
+  # @return [Hash] if the website was scraped correctly
   def initialize(uri = 'https://www.joingrouper.com/', *junk)
     @data = {}
     @success = []
     @failure = []
     @domain = uri.chomp('/')
     merge uri, [], [], [], []
-    @success.empty? ? FALSE : super
+    @success.empty? ? FALSE : @data
   end
 
+  # Merge the URI and associated data into collection
+  # @param uri [String] the URI that was scraped
+  # @param a [Array] links from the URI
+  # @param js [Array] javascript from the URI
+  # @param css [Array] css from the URI
+  # @param img [Array] images from the URI
+  # @return [Hash] entire collection of data
   def merge(uri, a, js, css, img)
     update = { uri => { a: a, js: js, css: css, img: img } }
     @data = @data.merge(update)
   end
 
+  # Download the URI
+  # @param uri [String] the URI to download
+  # @return [String] the content of the URI
   def download(uri)
     Nokogiri::HTML(open(uri))
   rescue
@@ -27,6 +41,11 @@ class Creep
     nil
   end
 
+  # Look through URI content for element attributes
+  # @param selector [String] the selector to use
+  # @param attribute [String] the attribute of the selector to safe
+  # @param content [String] the content to search through
+  # @return [String] a list of the element attributes
   def look_for(selector, attribute, content)
     list = []
     content.css(selector).each do |e|
@@ -36,6 +55,10 @@ class Creep
     list
   end
 
+  # Validate URI
+  # @param uri [String] the URI to validate
+  # @return [String] if the URI can be validated
+  # @return [NilClass] if the URI can't be validated
   def audit_uri(uri)
     if uri.is_a? String
       if uri.start_with?(@domain)
@@ -49,6 +72,10 @@ class Creep
     end
   end
 
+  # Parses content and delegates element attribute search
+  # @param content [String] the content to parse 
+  # @return [Array] if there is content to search
+  # @return [NilClass] if there is no content to search
   def parse(content)
     unless content.nil?
       [
@@ -60,19 +87,28 @@ class Creep
     end
   end
 
-  def mine(key)
-    if !@success.include?(key) && !@failure.include?(key)
-      result = parse(download(key))
+  # Mine URI for other links
+  # @param uri [String] the URI to mine
+  # @return [TrueClass] if the URI is mined successfully
+  # @return [FalseClass] if the URI can't be mined
+  def mine(uri)
+    if !@success.include?(uri) && !@failure.include?(uri)
+      result = parse(download(uri))
       unless result.nil?
-        merge key, *result
-        @success.push key
+        merge uri, *result
+        @success.push uri
         result[0].each do |link|
           merge(link, [], [], [], []) unless @data.include? link
         end
+        TRUE
       end
+    else
+      FALSE
     end
   end
 
+  # Scrape
+  # @return [Hash] the full collection of mined data
   def scrape
     progress = 0
     until progress == @data.length
